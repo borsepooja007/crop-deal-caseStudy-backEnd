47084b153291faac3bd3552bceb63131c2f12eb6
borsepooja007 <59304709+borsepooja007@users.noreply.github.com>
2023-10-30 18:22:15 +0530

    Admin Service

---
+++ Admin_serviceMain/Admin_serviceMain/src/main/java/Admin_Service/Admin_serviceMain/model/Payment_Method.java
@@ -0,0 +1,44 @@
+package Admin_Service.Admin_serviceMain.model;
+
+public class Payment_Method {
+	private String card_number;
+	private String card_type;
+	private String cvv;
+
+	public Payment_Method() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Payment_Method(String card_number, String card_type, String cvv) {
+		super();
+		this.card_number = card_number;
+		this.card_type = card_type;
+		this.cvv = cvv;
+	}
+
+	public String getCard_number() {
+		return card_number;
+	}
+
+	public void setCard_number(String card_number) {
+		this.card_number = card_number;
+	}
+
+	public String getCard_type() {
+		return card_type;
+	}
+
+	public void setCard_type(String card_type) {
+		this.card_type = card_type;
+	}
+
+	public String getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+
+}
---
+++ Admin_serviceMain/Admin_serviceMain/src/test/java/Admin_Service/Admin_serviceMain/AdminServiceMainApplicationTests.java
@@ -0,0 +1,13 @@
+package Admin_Service.Admin_serviceMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
