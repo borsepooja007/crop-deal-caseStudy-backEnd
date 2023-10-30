84aaeb7d7cc5af83e487771cb6f8ece1cf09873b
borsepooja007 <59304709+borsepooja007@users.noreply.github.com>
2023-10-30 18:24:09 +0530

    Delete Admin_serviceMain/Admin_serviceMain directory

--- Admin_serviceMain/Admin_serviceMain/src/main/java/Admin_Service/Admin_serviceMain/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package Admin_Service.Admin_serviceMain.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import Admin_Service.Admin_serviceMain.model.Admin;
-
-public interface AdminRepository extends MongoRepository<Admin, String> {
-
-}
